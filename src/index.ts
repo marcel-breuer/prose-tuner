a883a7c21b47b331167ebb74eba00290bcb79f73
Marcel Breuer <breuer.marcel@outlook.com>
2026-08-16 11:27:21 +0200

    feat(parser): protect Markdown segments

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@
  * Domain capabilities are introduced incrementally in subsequent milestones.
  */
 export const VERSION = '0.1.0';
+
+export * from './parser/index.js';
---
+++ src/parser/index.ts
@@ -0,0 +1,17 @@
+export { detectInputFormat, parseDocument } from './parse-document.js';
+export {
+  EditableReplacementCountError,
+  mapEditableSegments,
+  renderDocument,
+} from './render-document.js';
+export type {
+  DocumentSegment,
+  EditableSegment,
+  InputFormat,
+  ParseDocumentOptions,
+  ParsedDocument,
+  ProtectedContentKind,
+  ProtectedSegment,
+  StructureSegment,
+  TextRange,
+} from './types.js';
